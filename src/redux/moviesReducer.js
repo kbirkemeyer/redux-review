2c9a955567e042cec2e78c027b280e343c5e9b19
Katie <adlerk8@gmail.com>
2020-11-10 09:32:11 -0700

    initial commmit

---
+++ src/redux/moviesReducer.js
@@ -0,0 +1,37 @@
+// Always good to have an initial state that contains as many of the keys as possible.
+const initialState = {
+    title: '',
+    poster: '',
+    rating: null,
+    movies: []
+};
+
+// Declare action constant (also called action type)
+const SET_MOVIE_INFO = 'SET MOVIE INFO';
+
+//Action builders exported from reducer - similar to this.setState
+// EVERY ACTION BUILDER NEEDS TO RETURN AN OBJECT WITH 2 PROPERTIES - TYPE & PAYLOAD
+export const setMovieInfo = (title, poster, rating) => {
+    return (
+        {
+            type: SET_MOVIE_INFO,
+            payload: {title, poster, rating}
+        }
+    )
+};
+
+// Actual reducer - should always be called the same thing as the file name
+// state = initialState means if state is undefined, pass in initialState and set it equal to state
+const moviesReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SET_MOVIE_INFO:
+            return {...state, ...action.payload}
+    }
+};
+
+export default moviesReducer;
+
+
+
+
+
